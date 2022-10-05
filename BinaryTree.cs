0fb778dec9b900705dd2387f0c05026da847c6a0
Ilona Lapka <ilona2000123@wp.pl>
2022-10-05 21:54:05 +0200

    Initial

---
+++ BinaryTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytm14
+{
+    public class BinaryTree
+    {
+        int value;
+        BinaryTree left;
+        BinaryTree right;
+
+        public BinaryTree(List<int> values) : this(values, 0) { }
+
+        BinaryTree(List<int> values, int index)
+        {
+            Load(this, values, index);
+        }
+
+        void Load(BinaryTree tree, List<int> values, int index)
+        {
+            this.value = values[index];
+            if (index * 2 + 1 < values.Count)
+            {
+                this.left = new BinaryTree(values, index * 2 + 1);
+            }
+            if (index * 2 + 2 < values.Count)
+            {
+                this.right = new BinaryTree(values, index * 2 + 2);
+            }
+        }
+
+        public void PrintInDepth(BinaryTree r, int depth)
+        {
+            if (r == null) return;
+            PrintInDepth(r.right, depth + 1);
+            for (int i = 0; i < depth; i++)
+            {
+                Console.Write("    ");
+            }
+            Console.WriteLine(r.value);
+            PrintInDepth(r.left, depth + 1);
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorytm14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> values = new List<int> { 2, 6, 1, 10, 3, 4, 7, 9, 8, 5 };
+            Console.WriteLine("Przykładowe drezwo binarne ma postać:");
+            BinaryTree tree = new BinaryTree(values);
+            tree.PrintInDepth(tree, 0);
+            int x, indexX, a, y, result;
+            do
+            {
+                Console.WriteLine("Podaj wierzchołek, którego kuzynów chcesz znaleźć: ");
+                x = Int32.Parse(Console.ReadLine());
+                indexX = values.IndexOf(x);
+                if (indexX == 0)
+                {
+                    Console.WriteLine("Podany wierzchołek nie ma kuzynów.");
+                }
+                else if(indexX == -1)
+                {
+                    Console.WriteLine("Podanego wierzchołka nie ma w drzewie.");
+                }
+                else
+                {
+                    a = 0;
+                    y = 1;
+                    do
+                    {
+                        a++;
+                        result = Convert.ToInt32(Math.Pow(2, a));
+                        y += result;
+                    } while (y < indexX + 1);
+                    Console.Write("Jego kuzyni to: ");
+                    for (int i = y - result; i < y && i < values.Count; i++)
+                    {
+                        if (i == indexX) continue;
+                        Console.Write(values[i] + " ");
+                    }
+                    Console.WriteLine("\n");
+                }
+
+            } while (true);
+        }
+    }
+}
